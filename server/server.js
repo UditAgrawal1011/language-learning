fbb681a11ff510b0662789afa135940be87a7513
UditAgrawal1011 <163242202+UditAgrawal1011@users.noreply.github.com>
2025-07-08 09:16:10 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -28,6 +28,9 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(5000, () => console.log("Server running on port 5000"));
+    app.listen(process.env.PORT || 5000, '0.0.0.0', () =>
+  console.log(`Server running on port ${process.env.PORT || 5000}`)
+);
+
   })
   .catch((err) => console.error("MongoDB connection error:", err));
